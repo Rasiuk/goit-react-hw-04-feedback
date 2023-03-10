e7e5af429fdfbca927a0420766cf690895cec527
Evgeniy <gerardcobain1@gmail.com>
2023-03-10 20:26:43 +0200

    complete homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,72 +1,54 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Statistic } from './Statistic/Statistic';
 import { OptionsBtn } from './FeedbackOptions/FeedbackOptions';
 import { Section } from 'Section/Section';
 import { Notification } from './Notification/Notificatin';
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  clickOnGood = () => {
-    this.setState(prevState => {
-      return { good: prevState.good + 1 };
-    });
-    this.countTotalFeedback();
-  };
-  clickOnNeutral = () => {
-    this.setState(prevState => {
-      return { neutral: prevState.neutral + 1 };
-    });
-    this.countTotalFeedback();
-  };
-  clickOnbad = () => {
-    this.setState(prevState => {
-      return { bad: prevState.bad + 1 };
-    });
-    this.countTotalFeedback();
-  };
-  countTotalFeedback = () => {
-    this.setState(prevState => {
-      return { total: prevState.good + prevState.neutral + prevState.bad };
-    });
-    this.countPositiveFeedbackPercentage();
-  };
-  countPositiveFeedbackPercentage() {
-    this.setState(prevState => {
-      let totalFeedback = Math.round((prevState.good * 100) / prevState.total);
-      return {
-        percentage: totalFeedback,
-      };
-    });
-  }
-  render() {
-    const { good, neutral, bad, total, percentage } = this.state;
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <OptionsBtn
-            clickOnGood={this.clickOnGood}
-            clickOnNeutral={this.clickOnNeutral}
-            clickOnbad={this.clickOnbad}
-          />
-        </Section>
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [percentage, setPercentage] = useState(0);
 
-        <Section title="Statistics">
-          {!good && !neutral && !bad ? (
-            <Notification message="There is no feedback" />
-          ) : (
-            <Statistic
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={percentage}
-            />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+  const clickOnGood = () => {
+    setGood(prevState => prevState + 1);
+  };
+  const clickOnNeutral = () => {
+    setNeutral(prevState => prevState + 1);
+  };
+  const clickOnbad = () => {
+    setBad(prevState => prevState + 1);
+  };
+  useEffect(() => {
+    setTotal(good + neutral + bad);
+  }, [good, neutral, bad]);
+  useEffect(() => {
+    setPercentage(Math.round((good * 100) / total));
+  }, [good, total]);
+
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <OptionsBtn
+          clickOnGood={clickOnGood}
+          clickOnNeutral={clickOnNeutral}
+          clickOnbad={clickOnbad}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {!good && !neutral && !bad ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistic
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={percentage}
+          />
+        )}
+      </Section>
+    </div>
+  );
+};
